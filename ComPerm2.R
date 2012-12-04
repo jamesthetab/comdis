c3797635936cb3a4eaae6e0254388e2c312ddc61
Max Joseph <maxwellbjoseph@gmail.com>
2012-12-04 14:24:55 -0700

    added cij as argument to ComPerm2, tweaked plotting function

--- ComPerm2.R
+++ ComPerm2.R
@@ -36,7 +36,7 @@
 
 ComPerm2 <- function(pool, kWeightPenalty, mode,
 										comsizes = 2:pool$Nglobal, iter = 10, Kmeth = "free",
-										Nperms = 10) {   
+										Nperms = 10, cij=0.05) {   
 	stopifnot(Kmeth == "fixed" | Kmeth == "free")
 	write("generating permutations...", "")
 	# Generate all possible Bii permutations from a global pool
@@ -93,7 +93,7 @@ ComPerm2 <- function(pool, kWeightPenalty, mode,
 	for (p in 1:number.perms){
 		globalpool <- subset(global.perms, permutation == p)
 		dummylist <- list(global.pool = globalpool) # because ComDis extracts the pool from a list
-		runp <- ComDis(dummylist, mode, iter, Kmeth, kWeightPenalty)
+		runp <- ComDis(dummylist, mode, iter, Kmeth, kWeightPenalty, cij)
 		permutation <- rep(p, nrow(runp$all.data))
 		inversions <- rep(R0.inversions[p], nrow(runp$all.data))
 		rund <- cbind(runp$all.data, permutation, inversions)
@@ -115,7 +115,9 @@ ComPerm2 <- function(pool, kWeightPenalty, mode,
 
 #------------------------------------------------------------------------------
 # Plotting function
-plot.ComPerm2 <- function(run, meth="eff"){
+plot.ComPerm2 <- function(run, meth="eff", 
+													alpha=ifelse(max(run$data$outer.iteration) <= 50, 1, 
+																			 (exp(-0.01 * max(run$data$outer.iteration)) + .05))){
 	stopifnot(meth == "traj" | meth == "eff")
 	require(ggplot2)
 	iter <- max(run$data[, 1])
@@ -126,8 +128,7 @@ plot.ComPerm2 <- function(run, meth="eff"){
 		return(ggplot(run$data) + theme_bw() +
 			geom_line(aes(richness, Ro, group = interaction(outer.iteration, permutation),
 										color=as.factor(permutation)),
-								alpha=ifelse(max(run$data$outer.iteration) <= 50, 1, 
-														 (exp(-0.01 * max(run$data$outer.iteration)) + .05))) +
+								alpha=alpha) +
 														 	facet_wrap(~inversions) +
 														 	theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank()) +
 														 	theme(legend.position="none") +
@@ -139,11 +140,11 @@ plot.ComPerm2 <- function(run, meth="eff"){
 															list(iter = iter))
 		return(ggplot(run$data) + theme_bw() + 
 			geom_jitter(aes(inversions, delta.Ro, color=inner.iteration),
-									alpha=ifelse(max(run$data$outer.iteration) <= 50, 1, 
-															 (exp(-0.01 * max(run$data$outer.iteration)) + .05)),
+									alpha=alpha,
 									position = position_jitter(height = .0001)) + 
 										scale_colour_gradient(low="blue", high="red") +
 										geom_hline(yintercept=0) +
+										geom_smooth(aes(x=inversions, y=delta.Ro, group=inner.iteration, color=inner.iteration)) +
 										theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank()) +
 										ggtitle(chart_title))
 	}
@@ -152,7 +153,8 @@ plot.ComPerm2 <- function(run, meth="eff"){
 
 #------------------------------------------------------------------------------
 # Example
-pool1 <- GPool(Nglobal=6, globmeth="allom", a=2)
-test <- ComPerm2(pool1, mode="freq", kWeightPenalty=2, iter=10, Kmeth="free")
+pool1 <- GPool(globmeth="allom", Nglobal=6)
+test <- ComPerm2(pool1, mode="freq", 
+								 kWeightPenalty=3, iter=10, Kmeth="free", cij=.05, Nperms=40)
 plot(test, meth="traj")
-plot(test, meth="eff")
+plot(test, meth="eff", alpha=0.3)
